21be672e76d4a8f21a48950e4bcf490a4f42970d
Siege365 <n.merka.544341@umindanao.edu.ph>
2024-11-19 23:15:19 +0800

    Transition Update

--- src/main/java/com/example/softwareproj/AppController.java
+++ src/main/java/com/example/softwareproj/AppController.java
@@ -1,7 +1,9 @@
 package com.example.softwareproj;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class AppController {
@@ -12,10 +14,52 @@ public class AppController {
     @FXML
     private Pane footer;
 
+    @FXML
+    private AnchorPane fp;
+
+    @FXML
+    private AnchorPane hp;
+
+    @FXML
+    private AnchorPane mp;
+
+    @FXML
+    private AnchorPane op;
+
 
     @FXML
     public void getUserData(String username) {
         Userlabel.setText("Hello, " + username + "!");
         //usertf.setText(username);
     }
+    @FXML
+    void toFood(ActionEvent event) {
+        fp.setVisible(true);
+        mp.setVisible(false);
+        op.setVisible(false);
+
+    }
+
+    @FXML
+    void toHome(ActionEvent event) {
+        fp.setVisible(false);
+        mp.setVisible(false);
+        op.setVisible(false);
+
+    }
+
+    @FXML
+    void toMore(ActionEvent event) {
+        fp.setVisible(false);
+        mp.setVisible(true);
+        op.setVisible(false);
+    }
+
+    @FXML
+    void toOrder(ActionEvent event) {
+        fp.setVisible(false);
+        mp.setVisible(false);
+        op.setVisible(true);
+    }
+
 }
